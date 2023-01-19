7407db126f69a786d3fad14bd968298ffdcea5cf
Artem Slotin <orient.ck@gmail.com>
2023-01-19 21:21:32 +0200

    add all closures tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -115,8 +115,23 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  let count = 0;
+  return function retried() {
+    let result;
+    while (count < attempts) {
+      try {
+        result = func();
+        break;
+      } catch (error) {
+        count += 1;
+        if (count === attempts) {
+          throw error;
+        }
+      }
+    }
+    return result;
+  };
 }
 
 
@@ -143,8 +158,13 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function logged(...args) {
+    logFunc(`${func.name}(${args.map((arg) => JSON.stringify(arg)).join(',')}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${args.map((arg) => JSON.stringify(arg)).join(',')}) ends`);
+    return result;
+  };
 }
 
 
@@ -161,8 +181,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function partialFn(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 
@@ -183,8 +205,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom - 1;
+  return function idGenerator() {
+    currentId += 1;
+    return currentId;
+  };
 }
 
 
