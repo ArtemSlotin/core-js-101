3d08d296b6d0f9b7e29eaca2183769caab20f51a
Artem Slotin <orient.ck@gmail.com>
2023-01-19 21:10:17 +0200

    add closures tasks untill memoize

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (num) => num ** exponent;
 }
 
 
@@ -62,8 +62,16 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...coeff) {
+  if (coeff.length === 0) return null;
+
+  return (x) => {
+    let y = 0;
+    for (let i = 0; i < coeff.length; i += 1) {
+      y += coeff[i] * (x ** (coeff.length - 1 - i));
+    }
+    return y;
+  };
 }
 
 
@@ -81,8 +89,14 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+  return function memoized(...args) {
+    if (!cache[args]) {
+      cache[args] = func(...args);
+    }
+    return cache[args];
+  };
 }
 
 
